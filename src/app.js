f20810d37d768549f7d7f9b7d8150478e7dff567
duyentth <duyen.tran@miu.edu>
2024-10-10 15:10:20 -0400

    added Cron Job

--- src/app.js
+++ src/app.js
@@ -5,17 +5,18 @@ import notificationRouter from "./routes/notification.route.js";
 import productRouter from "./routes/product.route.js";
 import bidRouter from "./routes/bid.route.js";
 import paymentRouter from "./routes/payment.route.js";
+import job from "./cron.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: [
-            "http://localhost:3000",
-            "https://refurbished-goods-marketplace-app.onrender.com",
-        ],
-    })
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://refurbished-goods-marketplace-app.onrender.com",
+    ],
+  })
 );
 
 app.use("/api/auth", authRouter);
@@ -24,17 +25,20 @@ app.use("/api/products", productRouter);
 app.use("/api/bids", bidRouter);
 app.use("/api/payments", paymentRouter);
 
+//starting the job sending GET request to server for every 14 minutes to make the server always active on Render.com
+job.start();
+
 //error handler
 app.use((error, req, res, next) => {
-    if (error) {
-        res.status(500).send("Server Error: ", error);
-    } else {
-        next();
-    }
+  if (error) {
+    res.status(500).send("Server Error: ", error);
+  } else {
+    next();
+  }
 });
 
 app.use((req, res) => {
-    res.status(501).send("API is not supported.");
+  res.status(501).send("API is not supported.");
 });
 
 export default app;
